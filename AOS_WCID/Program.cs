d492bca8a347bba64a3e5418fe42e00006ca7ab6
MaikaQuizzel <70568319+MaikaQuizzel@users.noreply.github.com>
2023-02-27 15:08:21 +0100

    Add Main
    Add Test and dependencies
    Add more function to gamemode choosing

--- AOS_WCID/Program.cs
+++ AOS_WCID/Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+
 Console.WriteLine("Hello, World!");
+EingabeGameMode();
+
+
+static void EingabeGameMode() { 
+    Console.WriteLine("Für welchen Spielmodus möchtest du erstellen");
+    Console.WriteLine("1 für Path to Glory \n 2 für normal");
+    string consoleEingabe = "2";
+    try
+    {
+        consoleEingabe = Console.ReadLine().ToString();
+    }
+    catch (Exception e) { }
+    finally { 
+        Console.WriteLine(consoleEingabe);
+    }
+    
+}
+
---
+++ WCID_UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using AOS_WCID;
+
+namespace WCID_UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(1 == 1);
+        }
+    }
+}
