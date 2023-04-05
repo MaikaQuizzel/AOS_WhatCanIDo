884468ced0678da8a10299d0bddbce8942c6704f
MaikaQuizzel <70568319+MaikaQuizzel@users.noreply.github.com>
2023-04-05 09:08:50 +0200

    Create ConsolenReaderTest.cs

---
+++ WCID_UnitTests/ConsolenReaderTest.cs
@@ -0,0 +1,43 @@
+﻿using AOS_WCID.Konsole;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCID_UnitTests
+{
+    [TestClass]
+    public class ConsolenReaderTest
+    {
+        [TestMethod]
+        public void TestGetLine()
+        {
+            // Arrange
+            string expected = "Hello World!";
+            ConsolenReader reader = new ConsolenReader();
+
+            // Act
+            Console.SetIn(new System.IO.StringReader(expected));
+            string actual = reader.GetLine();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestGetLineEmpty()
+        {
+            // Arrange
+            string expected = "";
+            ConsolenReader reader = new ConsolenReader();
+
+            // Act
+            Console.SetIn(new System.IO.StringReader(""));
+            string actual = reader.GetLine();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
