fcfe8dbe4c4c23e01fcbee069f799efdd9fde9ee
MaikaQuizzel <70568319+MaikaQuizzel@users.noreply.github.com>
2023-04-18 10:48:19 +0200

    Comparator for Words in abilities

---
+++ AOS_WCID/Logic/SpecialWordsComparator.cs
@@ -0,0 +1,63 @@
+﻿using AOS_WCID.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOS_WCID.Logic
+{
+    public  class SpecialWordsComparator
+    {
+        private List<string> wordsHeroPhase;
+        private List<string> wordsMovmentPhase;
+        private List<string> wordsChargePhase;
+        private List<string> wordsShootingPhase;
+        private List<string> wordsCombatPhase;
+        private List<string> wordsBattleshockPhase;
+
+        public SpecialWordsComparator() 
+        {
+
+        }
+
+        public bool CompareAbilitiyToList(string ability, string phase) 
+        {
+            var listToCompare = new List<string>();
+            if(phase == StringConstants.HEROPHASE)
+            {
+                listToCompare.AddRange(wordsHeroPhase);
+            }
+
+            if (phase == StringConstants.MOVEMENTPHASE)
+            {
+                listToCompare.AddRange(wordsMovmentPhase);
+            }
+
+            if (phase == StringConstants.CHARGEPHASE)
+            {
+                listToCompare.AddRange(wordsChargePhase);
+            }
+
+            if (phase == StringConstants.SHOOTINGPHASE)
+            {
+                listToCompare.AddRange(wordsShootingPhase);
+            }
+
+            if(phase == StringConstants.COMBATPHASE)
+            {
+                listToCompare.AddRange(wordsCombatPhase);
+            }
+
+            if (phase == StringConstants.BATTLESHOCKPHASE)
+            {
+                listToCompare.AddRange(wordsBattleshockPhase);
+            }
+            return false;    
+        }
+        private void InitWordLists()
+        {
+
+        }
+    }
+}
