fac4837bf9ace9aff48a4c05e5fddc7958733c41
MaikaQuizzel <70568319+MaikaQuizzel@users.noreply.github.com>
2023-04-18 10:46:21 +0200

    new Constants for Phases
    new Constands

--- AOS_WCID/Data/StringConstants.cs
+++ AOS_WCID/Data/StringConstants.cs
@@ -10,5 +10,11 @@ namespace AOS_WCID.Data
     {
         public readonly static string GAMEMODEPATH = "Path to glory";
         public readonly static string NOSUBFACTION = "No Subfaction";
+        public readonly static string HEROPHASE = "hero";
+        public readonly static string MOVEMENTPHASE = "movement";
+        public readonly static string CHARGEPHASE = "charge";
+        public readonly static string SHOOTINGPHASE = "shooting";
+        public readonly static string COMBATPHASE = "combat";
+        public readonly static string BATTLESHOCKPHASE = "battleshock";
     }
 }
